78c72417dc36caf704c01325880e7d2f776b03e1
Foldedgit <shahmoradi.iman@gmail.com>
2023-05-09 21:19:29 +0200

    unnecessary headers removed

--- Pass.hpp
+++ Pass.hpp
@@ -1,12 +1,12 @@
 #ifndef PASS_HPP
 #define PASS_HPP
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iostream>
-#include <string>
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//#include <iostream>
+//#include <string>
 #include <regex>
 
 #include "Utifac.hpp"
@@ -16,7 +16,6 @@
 bool PassCreate(std::string HASHpk, std::string HASHsk, std::string Secret_phrase);
 std::shared_ptr<BGVrns> PassCheck(std::string Secret_Phrase, std::string keyroot);
 std::string Secret_to_Hashs(std::string Secret_Phrase);
-
 
 
 #endif
